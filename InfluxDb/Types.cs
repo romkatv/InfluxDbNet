b5ef2b4e3211eb03d1e4597fd57312be2f8b8f14
romkatv <roman.perepelitsa@gmail.com>
2016-08-21 12:32:49 +0200

    Support non-double fields. Add missing timestamp field.

--- InfluxDb/Types.cs
+++ InfluxDb/Types.cs
@@ -6,22 +6,45 @@ using System.Threading.Tasks;
 
 namespace InfluxDb
 {
-    public class Tag
+    public abstract class Field : Variant<long, double, bool, string>
     {
-        public string Name { get; set; }
-        public string Value { get; set; }
+        public static new Field New(long val)
+        {
+            return new Instance(Variant<long, double, bool, string>.New(val));
+        }
+        public static new Field New(double val)
+        {
+            return new Instance(Variant<long, double, bool, string>.New(val));
+        }
+        public static new Field New(bool val)
+        {
+            return new Instance(Variant<long, double, bool, string>.New(val));
+        }
+        public static new Field New(string val)
+        {
+            return new Instance(Variant<long, double, bool, string>.New(val));
+        }
+
+        sealed class Instance : Field
+        {
+            readonly Variant<long, double, bool, string> _var;
+            public Instance(Variant<long, double, bool, string> var)
+            {
+                _var = var;
+            }
+            public override R Visit<R>(Func<long, R> v0, Func<double, R> v1, Func<bool, R> v2, Func<string, R> v3)
+            {
+                return _var.Visit(v0, v1, v2, v3);
+            }
+        }
     }
 
-    public class Field
-    {
-        public string Name { get; set; }
-        public double Value { get; set; }
-    }
 
     public class Point
     {
         public string Name { get; set; }
-        public SortedDictionary<string, Tag> Tags { get; set; }
+        public DateTime Timestamp { get; set; }
+        public SortedDictionary<string, string> Tags { get; set; }
         public SortedDictionary<string, Field> Fields { get; set; }
     }
 

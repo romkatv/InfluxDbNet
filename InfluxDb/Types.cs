aefa2234a614c8cd58e5b42ee2cb1dde5629f5ee
romkatv <roman.perepelitsa@gmail.com>
2016-08-28 17:16:53 +0200

    Remove Variant.

--- InfluxDb/Types.cs
+++ InfluxDb/Types.cs
@@ -6,35 +6,106 @@ using System.Threading.Tasks;
 
 namespace InfluxDb
 {
-    public abstract class Field : Variant<long, double, bool, string>
+    public abstract class Field : IEquatable<Field>
     {
-        public static new Field New(long val)
+        public static Field New(long val)
         {
-            return new Instance(Variant<long, double, bool, string>.New(val));
+            return new C0() { Val = val };
         }
-        public static new Field New(double val)
+        public static Field New(double val)
         {
-            return new Instance(Variant<long, double, bool, string>.New(val));
+            return new C1() { Val = val };
         }
-        public static new Field New(bool val)
+        public static Field New(bool val)
         {
-            return new Instance(Variant<long, double, bool, string>.New(val));
+            return new C2() { Val = val };
         }
-        public static new Field New(string val)
+        public static Field New(string val)
         {
-            return new Instance(Variant<long, double, bool, string>.New(val));
+            return new C3() { Val = val };
         }
 
-        sealed class Instance : Field
+        /// <summary>
+        /// Calls exactly one of the passed functions passing it the currently held value.
+        /// </summary>
+        public abstract R Visit<R>(Func<long, R> v0, Func<double, R> v1, Func<bool, R> v2, Func<string, R> v3);
+
+        /// <summary>
+        /// Zero-based index of the active value.
+        /// </summary>
+        public int Index()
         {
-            readonly Variant<long, double, bool, string> _var;
-            public Instance(Variant<long, double, bool, string> var)
-            {
-                _var = var;
-            }
+            return Visit((T0) => 0, (T1) => 1, (T2) => 2, (T3) => 3);
+        }
+
+        /// <summary>
+        /// Type of the active value.
+        /// </summary>
+        public Type Type()
+        {
+            return Visit((T0) => typeof(long), (T1) => typeof(double), (T2) => typeof(bool), (T3) => typeof(string));
+        }
+
+        /// <summary>
+        /// Currently active value as Object.
+        /// </summary>
+        public object Value()
+        {
+            return Visit<object>((long v) => v, (double v) => v, (bool v) => v, (string v) => v);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}: {1}", Type().FullName, Value());
+        }
+
+        public bool Equals(Field other)
+        {
+            if (other == null) return false;
+            if (Index() != other.Index()) return false;
+            return Object.Equals(Value(), other.Value());
+        }
+
+        public override bool Equals(Object obj)
+        {
+            return Equals(obj as Field);
+        }
+
+        public override int GetHashCode()
+        {
+            return Hash.HashWithSeed(Index(), Value());
+        }
+
+        sealed class C0 : Field
+        {
+            public long Val;
             public override R Visit<R>(Func<long, R> v0, Func<double, R> v1, Func<bool, R> v2, Func<string, R> v3)
             {
-                return _var.Visit(v0, v1, v2, v3);
+                return v0.Invoke(Val);
+            }
+        }
+        sealed class C1 : Field
+        {
+            public double Val;
+            public override R Visit<R>(Func<long, R> v0, Func<double, R> v1, Func<bool, R> v2, Func<string, R> v3)
+            {
+                return v1.Invoke(Val);
+            }
+        }
+        sealed class C2 : Field
+        {
+            public bool Val;
+            public override R Visit<R>(Func<long, R> v0, Func<double, R> v1, Func<bool, R> v2, Func<string, R> v3)
+            {
+                return v2.Invoke(Val);
+            }
+        }
+        sealed class C3 : Field
+        {
+            public string Val;
+            public override R Visit<R>(Func<long, R> v0, Func<double, R> v1, Func<bool, R> v2, Func<string, R> v3)
+            {
+                return v3.Invoke(Val);
             }
         }
     }

5f0d48fcd97525c2d7b698da3880c09bd2a2f7e9
romkatv <roman.perepelitsa@gmail.com>
2016-08-28 12:16:25 +0200

    Support user-provided equality comparer.

--- InfluxDb/SequenceComparer.cs
+++ InfluxDb/SequenceComparer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Conditions;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace InfluxDb
@@ -8,11 +9,24 @@ namespace InfluxDb
     /// </summary>
     public class SequenceComparer<T> : IEqualityComparer<IEnumerable<T>>
     {
+        readonly IEqualityComparer<T> _cmp;
+
+        public SequenceComparer()
+        {
+            _cmp = EqualityComparer<T>.Default;
+        }
+
+        public SequenceComparer(IEqualityComparer<T> cmp)
+        {
+            Condition.Requires(cmp, "cmp").IsNotNull();
+            _cmp = cmp;
+        }
+         
         public bool Equals(IEnumerable<T> x, IEnumerable<T> y)
         {
             if (x == null) return y == null;
             if (y == null) return x == null;
-            return x.SequenceEqual(y);
+            return x.SequenceEqual(y, _cmp);
         }
 
         public int GetHashCode(IEnumerable<T> seq)
@@ -21,9 +35,15 @@ namespace InfluxDb
             int res = -1623057131;  // Random number.
             foreach (T elem in seq)
             {
-                res = Hash.HashWithSeed(res, elem);
+                res = Hash.HashWithSeed(res, HashElem(elem));
             }
             return res;
+        }
+
+        int HashElem(T elem)
+        {
+            if (elem == null) return -1921273924;  // Random number.
+            return _cmp.GetHashCode(elem);
         }
     }
 }

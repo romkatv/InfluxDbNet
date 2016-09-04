2e0f46c4982476d22242071432d2885d727bd530
romkatv <roman.perepelitsa@gmail.com>
2016-09-04 14:36:54 +0200

    Make MergeFrom() operate on any IDictionary.

--- InfluxDb/DictionaryExtension.cs
+++ InfluxDb/DictionaryExtension.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace InfluxDb
 {
-    static class SortedDictionaryExtension
+    static class DictionaryExtension
     {
         // The left argument is mutated.
         // The right argument wins in case of key conflicts.
         public static void MergeFrom<TKey, TValue>(
-            this SortedDictionary<TKey, TValue> x, SortedDictionary<TKey, TValue> y)
+            this IDictionary<TKey, TValue> x, IDictionary<TKey, TValue> y)
         {
             foreach (var kv in y)
             {

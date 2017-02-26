a0964a2a9f7bd9dd75410f64705790bdbc1dcb24
romkatv <roman.perepelitsa@gmail.com>
2017-02-26 11:10:16 +0100

    Evict old log messages from memory.

--- InfluxDb/LoggerExtension.cs
+++ InfluxDb/LoggerExtension.cs
@@ -1,4 +1,5 @@
-﻿using NLog;
+﻿using Conditions;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,16 @@ using System.Threading.Tasks;
 
 namespace InfluxDb
 {
-    static class LoggerExtension
+    public static class LoggerExtension
     {
         static readonly object _monitor = new object();
-        static readonly Dictionary<Tuple<Reference<Logger>, string>, DateTime> _lastLogTime
-            = new Dictionary<Tuple<Reference<Logger>, string>, DateTime>();
+        static readonly HashSet<Tuple<Reference<Logger>, string>> _set =
+            new HashSet<Tuple<Reference<Logger>, string>>();
+        // DateTime is in non-decreasing order. It's the log time.
+        // Invariant: each combination of logger (Item1) and message (Item2) is present at most once.
+        // Invariant: there is trivial one-to-one correspondence between the elements of _set and _queue.
+        static readonly Queue<Tuple<Reference<Logger>, string, DateTime>> _queue =
+            new Queue<Tuple<Reference<Logger>, string, DateTime>>();
 
         // {log, message} is the key.
         // Does nothing if a log messages with the same key has been written less than `period` ago.
@@ -27,12 +33,17 @@ namespace InfluxDb
         static bool ShouldLog(Logger log, string message, TimeSpan period)
         {
             DateTime now = DateTime.UtcNow;
+            DateTime cutoff = now - period;
             var key = Tuple.Create(new Reference<Logger>(log), message);
             lock (_monitor)
             {
-                DateTime last;
-                if (_lastLogTime.TryGetValue(key, out last) && now - last < period) return false;
-                _lastLogTime[key] = now;
+                while (_queue.Count > 0 && _queue.Peek().Item3 <= cutoff)
+                {
+                    var top = _queue.Dequeue();
+                    Condition.Requires(_set.Remove(Tuple.Create(top.Item1, top.Item2))).IsTrue();
+                }
+                if (!_set.Add(key)) return false;
+                _queue.Enqueue(Tuple.Create(key.Item1, key.Item2, now));
                 return true;
             }
         }
